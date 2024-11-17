d083b0e9246cb98b97e14cedfe7d6d24d4fdb2d3
Yair Andrade <y.andrade1@uniandes.edu.co>
2024-11-17 18:54:34 -0500

    firt commit

---
+++ src/main/java/uniandes/edu/co/SuperAndesNoSQL/SuperAndesNoSqlApplication.java
@@ -0,0 +1,13 @@
+package uniandes.edu.co.SuperAndesNoSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperAndesNoSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperAndesNoSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uniandes/edu/co/SuperAndesNoSQL/SuperAndesNoSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package uniandes.edu.co.SuperAndesNoSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperAndesNoSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
